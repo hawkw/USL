1dd87e1a78dc40b1fc2724c9a5c499316c97304c
ArcticLight <mattrulz127@gmail.com>
2014-11-05 19:24:51 -0500

    Fix the way string literals were being handled

--- src/main/scala/USL.scala
+++ src/main/scala/USL.scala
@@ -1,4 +1,4 @@
-import scala.util.parsing.combinator.RegexParsers
+import scala.util.parsing.combinator.JavaTokenParsers
 import scala.math.Numeric
 
 /**
@@ -8,16 +8,20 @@ abstract class Data
 case class Obj(data: List[Data]) extends Data {
   override def toString = s"{ ${data.mkString(" ")} }"
 }
-case class Num(n: Double) extends Data
-case class Lit(s: String) extends Data
+case class Num(n: Double) extends Data {
+  override def toString = n.toString
+}
+case class Lit(s: String) extends Data{
+  override def toString = s"'$s'"
+}
 
-object USL extends RegexParsers {
+object USL extends JavaTokenParsers {
 
-  def prog: Parser[Obj] = rep(obj|num|strlit|lit|whiteSpace) ^^ {case l => Obj(for {n <- l if n.isInstanceOf[Data]} yield {n.asInstanceOf[Data]}) }
+  def prog: Parser[Obj] = rep(obj|num|strlit|lit) ^^ {case l => Obj(for {n <- l if n.isInstanceOf[Data]} yield {n.asInstanceOf[Data]}) }
 
   def num: Parser[Num] = """[0-9]+\.?[0-9]*""".r ^^{ case n => Num(n.toDouble)}
-  def strlit: Parser[Obj] = "\"" ~> rep(".") <~ "\"" ^^{ case s => new Obj(s.map(Lit(_))) }
-  def lit: Parser[Lit] = """[^\s"{}]+[\s"{}]""".r ^^{case s => Lit(s.trim())}
+  def strlit: Parser[Obj] = stringLiteral ^^{ case s => Obj(s.toList.reverse.map({c => Lit(c.toString)}))}
+  def lit: Parser[Lit] = """[^{}"]+[{}]""".r ^^{case s => Lit(s.trim())}
   def obj: Parser[Obj] = "{" ~> rep(num|strlit|lit|obj) <~ "}" ^^{ case l => Obj(l)}
 
   def main(args: Array[String]): Unit = {
