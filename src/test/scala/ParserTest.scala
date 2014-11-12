d51145d032915c38169b1744cd33a61361bb2dd5
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-12 14:24:20 -0500

    Add LaTeX gitignore

---
+++ src/test/scala/ParserTest.scala
@@ -0,0 +1,10 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Created by hawk on 11/5/14.
+ */
+class ParserTest extends FlatSpec with Matchers {
+
+  "The parser" should "correctly parse"
+
+}
